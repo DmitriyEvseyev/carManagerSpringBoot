faf0c7124c161b21f1b1e50b95dc1e600748de2f
Dmitriy <yevseyevdmitriy@yandex.ru>
2024-10-14 13:02:34 +0400

    ConverterDTOTest

--- src/test/java/com/dmitriyevseyev/carmanagerspringboot/utils/ConverterDTOTest.java
+++ src/test/java/com/dmitriyevseyev/carmanagerspringboot/utils/ConverterDTOTest.java
@@ -24,9 +24,11 @@ class ConverterDTOTest {
     private CarDealershipDTO dealerDTO;
     private CarDealership dealer;
     private CarDealershipEntity dealerEntity;
-    List <CarDealershipDTO> dealerDTOList;
-    List <CarDealershipEntity> dealerEntityList;
-
+    List<CarDealership> dealersList;
+    List<CarDealershipDTO> dealerDTOList;
+    List<CarDealershipEntity> dealersEntityList;
+    List<Car> carsList;
+    List<CarDTO> carDTOList;
 
     @BeforeEach
     void setUp() throws ParseException {
@@ -56,20 +58,46 @@ class ConverterDTOTest {
                 color("white").isAfterCrash(false).build();
         this.carEntity = CarEntity.builder().
                 id(1).
-               // dealer(dealerEntity).
                 name("BMW").
                 date(formatter.parse("2023-01-01")).
                 color("white").
                 isAfterCrash(false).build();
+        dealersList = new ArrayList<>();
         dealerDTOList = new ArrayList<>();
-        dealerEntityList  = new ArrayList<>();
+        dealersEntityList = new ArrayList<>();
+        carsList = new ArrayList<>();
+        carDTOList = new ArrayList<>();
+
+        dealersList.add(dealer);
         dealerDTOList.add(dealerDTO);
-        dealerEntityList.add(dealerEntity);
+        dealersEntityList.add(dealerEntity);
+        carsList.add(car);
+        carDTOList.add(carDTO);
+
+    }
+    @Test
+    void convertCarsListToCarsDTOListTest() {
+        List<CarDTO> carDTOListReturn = converterDTO.convertCarsListToCarsDTOList(carsList);
+        System.out.println(carDTOListReturn);
+        Assertions.assertEquals(carDTOListReturn.size(), carDTOList.size());
+        Assertions.assertEquals(carDTOListReturn, carDTOList);
     }
 
     @Test
-    void convertDealerDTOListToDealerEntityListTest () {
-
+    void  convertDealersListToDealersDTOListTest (){
+        List<CarDealershipDTO> dealerDTOsListReturn =
+                converterDTO.convertDealersListToDealersDTOList(dealersList);
+        System.out.println(dealerDTOsListReturn);
+        Assertions.assertEquals(dealerDTOsListReturn.size(), dealerDTOList.size());
+        Assertions.assertEquals(dealerDTOsListReturn, dealerDTOList);
+    }
+    @Test
+    void convertDealerDTOListToDealerEntityListTest() {
+        List<CarDealershipEntity> dealerEntityListReturn =
+                converterDTO.convertDealerDTOListToDealerEntityList(dealerDTOList);
+        System.out.println(dealerEntityListReturn);
+        Assertions.assertEquals(dealerDTOList.size(), dealersEntityList.size());
+        Assertions.assertEquals(dealersEntityList, dealerEntityListReturn);
     }
 
     @Test
@@ -79,6 +107,9 @@ class ConverterDTOTest {
         Assertions.assertNotNull(carEntityReturn);
         Assertions.assertEquals(carEntity.getId(), carEntityReturn.getId());
         Assertions.assertEquals(carEntity.getName(), carEntityReturn.getName());
+        Assertions.assertEquals(carEntity.getDate(), carEntityReturn.getDate());
+        Assertions.assertEquals(carEntity.getColor(), carEntityReturn.getColor());
+        Assertions.assertEquals(carEntity.isAfterCrash(), carEntityReturn.isAfterCrash());
         Assertions.assertInstanceOf(CarEntity.class, carEntityReturn);
     }
 
@@ -89,6 +120,7 @@ class ConverterDTOTest {
         Assertions.assertNotNull(dealerDTOReturn);
         Assertions.assertEquals(dealer.getId(), dealerDTOReturn.getId());
         Assertions.assertEquals(dealer.getName(), dealerDTOReturn.getName());
+        Assertions.assertEquals(dealer.getAddress(), dealerDTOReturn.getAddress());
         Assertions.assertInstanceOf(CarDealershipDTO.class, dealerDTOReturn);
     }
 
@@ -99,6 +131,7 @@ class ConverterDTOTest {
         Assertions.assertNotNull(dealerReturn);
         Assertions.assertEquals(dealerDTO.getId(), dealerReturn.getId());
         Assertions.assertEquals(dealerDTO.getName(), dealerReturn.getName());
+        Assertions.assertEquals(dealer.getAddress(), dealerReturn.getAddress());
         Assertions.assertInstanceOf(CarDealership.class, dealerReturn);
     }
 
@@ -108,7 +141,11 @@ class ConverterDTOTest {
         System.out.println(carDTOReturn);
         Assertions.assertNotNull(carDTOReturn);
         Assertions.assertEquals(carDTO.getId(), carDTOReturn.getId());
+        Assertions.assertEquals(carDTO.getDealerId(), carDTOReturn.getDealerId());
         Assertions.assertEquals(carDTO.getName(), carDTOReturn.getName());
+        Assertions.assertEquals(carDTO.getDate(), carDTOReturn.getDate());
+        Assertions.assertEquals(carDTO.getColor(), carDTOReturn.getColor());
+        Assertions.assertEquals(carDTO.isAfterCrash(), carDTOReturn.isAfterCrash());
         Assertions.assertInstanceOf(CarDTO.class, carDTOReturn);
     }
 
@@ -118,15 +155,11 @@ class ConverterDTOTest {
         System.out.println(carReturn);
         Assertions.assertNotNull(carReturn);
         Assertions.assertEquals(car.getId(), carReturn.getId());
+        Assertions.assertEquals(car.getDealerId(), carReturn.getDealerId());
         Assertions.assertEquals(car.getName(), carReturn.getName());
+        Assertions.assertEquals(car.getDate(), carReturn.getDate());
+        Assertions.assertEquals(car.getColor(), carReturn.getColor());
+        Assertions.assertEquals(car.isAfterCrash(), carReturn.isAfterCrash());
         Assertions.assertInstanceOf(Car.class, carReturn);
-    }
-
-    @Test
-    void summTest() {
-        Integer b = converterDTO.sum(30);
-        System.out.println("SUM - " + b);
-
-        Assertions.assertEquals(60, b);
     }
 }
